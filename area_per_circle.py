16b0478404b59bcf46e1e672517243c7d94c0e36
Kestrel Bryce <Madeline.Bryce@stu.ocsb.ca>
2022-09-21 17:47:45 +0000

    “finished”

---
+++ area_per_circle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Created By: Kestrel Bryce
+# Date: Sep. 21, 2022
+# This program calculates the area and perimeter of a circle
+#   with a radius of 30 mm
+
+
+import math
+
+
+def main():
+    print("For a circle with a radius")
+    print("of 30 mm:")
+    print("")
+    print("Area = {} mm^2". format(math.pi*30**2))
+    print("Circumference = {} mm". format(2*math.pi*30))
+
+
+if __name__ == "__main__":
+    main()
